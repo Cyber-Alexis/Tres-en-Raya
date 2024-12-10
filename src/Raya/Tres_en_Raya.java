774e473fb7e52e8bdad7870abffe75fdff723282
Cyber-Alexis <alexis.n.buza@alumnes.ilerna.com>
2024-12-11 00:32:17 +0100

    Parte 2
    En este apartado lo que he hecho a sido utilizar un for para que vaya recorriendo las filas del tablero y luego el for de debajo que sirve para recorrer tambien las columnas del tablero y por ultimo lo que he puesto a sido unos system.out para preguntar los nombres los cuales estan relacionados con el J1 y el J2

--- src/Raya/Tres_en_Raya.java
+++ src/Raya/Tres_en_Raya.java
@@ -8,11 +8,22 @@ public class Tres_en_Raya {
 		Scanner x = new Scanner(System.in);
         Random r = new Random();
 
-        // Declaración de variables
+        // Declarar las variables
         char[][] tabla = new char[3][3];
         String j1, j2;
         int jugadas = 0, ganador = 0, turno;
 
+        // Aqui inicializare el juego
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tabla[i][j] = '-';
+            }
+        }
+        // Este apartado sirve para solicitar nombres
+        System.out.print("¿Cómo te llamas, jugador 1?: ");
+        j1 = x.nextLine();
+        System.out.print("¿Cómo te llamas, jugador 2?: ");
+        j2 = x.nextLine();
 		        
 	}
 }
