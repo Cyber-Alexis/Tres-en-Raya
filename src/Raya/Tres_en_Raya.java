29c09b7fe559e75373aa128e5773a0ae4a6e4048
Cyber-Alexis <alexis.n.buza@alumnes.ilerna.com>
2024-12-10 20:17:02 +0100

    Parte 1
    He añadido el Scanner y el Random, para leer la entrada del usuario y el random para generar numeros aleatorios, despues he declarado variables con una matriz de char que servira para almacenar las posiciones del tablero, luego he declarado las variables de string para indicar a cada jugador y luego con el int he declarado las variables de cada jugadas, ganador y turno

--- src/Raya/Tres_en_Raya.java
+++ src/Raya/Tres_en_Raya.java
@@ -5,8 +5,14 @@ public class Tres_en_Raya {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Hola
-		
-	}
+		Scanner x = new Scanner(System.in);
+        Random r = new Random();
 
+        // Declaración de variables
+        char[][] tabla = new char[3][3];
+        String j1, j2;
+        int jugadas = 0, ganador = 0, turno;
+
+		        
+	}
 }
